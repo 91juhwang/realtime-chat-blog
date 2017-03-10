26a798f456d169d8f07bb498b1de31ea492da223
James Hwang <91juhwang@gmail.com>
2017-03-10 17:47:38 -0500

    Link Articles to a User, Modify creating_article_specs

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.feature 'Creating articles' do
   before do
     @user = User.create(email: 'abc@gmail.com', password: '123456')
-    login_as(@user, scope: :user)
+    login_as(@user)
   end
 
   scenario 'A user creates a new article' do
    #do something in Bob's browser session
+    visit root_path
     click_link 'New Articles'
 
     fill_in 'Title', with: 'Creating a blog'
@@ -15,12 +16,14 @@ RSpec.feature 'Creating articles' do
 
     click_button 'Create Article'
 
+    expect(Article.last.user).to eq(@user)
     expect(page).to have_content('Article has been created')
     expect(page.current_path).to eq(articles_path)
     expect(page).to have_content("Created by: #{@user.email}")
   end
 
   scenario 'A user fails to create a new article' do
+    visit root_path
     click_link 'New Articles'
 
     fill_in 'Title', with: ''
